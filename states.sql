6f2b826ece73b49bae9f1c44561bdfdf445559c1
Sean Walsh <sean.robert.walsh@gmail.com>
2016-01-10 11:00:01 -0600

    SQL

---
+++ states.sql
@@ -0,0 +1,72 @@
+CREATE TABLE IF NOT EXISTS state (
+  id INT NOT NULL auto_increment,
+  name CHAR(40) NOT NULL,
+  abbrev CHAR(2) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO state VALUES (NULL, 'Alaska', 'AK');
+INSERT INTO state VALUES (NULL, 'Alabama', 'AL');
+INSERT INTO state VALUES (NULL, 'American Samoa', 'AS');
+INSERT INTO state VALUES (NULL, 'Arizona', 'AZ');
+INSERT INTO state VALUES (NULL, 'Arkansas', 'AR');
+INSERT INTO state VALUES (NULL, 'California', 'CA');
+INSERT INTO state VALUES (NULL, 'Colorado', 'CO');
+INSERT INTO state VALUES (NULL, 'Connecticut', 'CT');
+INSERT INTO state VALUES (NULL, 'Delaware', 'DE');
+INSERT INTO state VALUES (NULL, 'District of Columbia', 'DC');
+INSERT INTO state VALUES (NULL, 'Federated state of Micronesia', 'FM');
+INSERT INTO state VALUES (NULL, 'Florida', 'FL');
+INSERT INTO state VALUES (NULL, 'Georgia', 'GA');
+INSERT INTO state VALUES (NULL, 'Guam', 'GU');
+INSERT INTO state VALUES (NULL, 'Hawaii', 'HI');
+INSERT INTO state VALUES (NULL, 'Idaho', 'ID');
+INSERT INTO state VALUES (NULL, 'Illinois', 'IL');
+INSERT INTO state VALUES (NULL, 'Indiana', 'IN');
+INSERT INTO state VALUES (NULL, 'Iowa', 'IA');
+INSERT INTO state VALUES (NULL, 'Kansas', 'KS');
+INSERT INTO state VALUES (NULL, 'Kentucky', 'KY');
+INSERT INTO state VALUES (NULL, 'Louisiana', 'LA');
+INSERT INTO state VALUES (NULL, 'Maine', 'ME');
+INSERT INTO state VALUES (NULL, 'Marshall Islands', 'MH');
+INSERT INTO state VALUES (NULL, 'Maryland', 'MD');
+INSERT INTO state VALUES (NULL, 'Massachusetts', 'MA');
+INSERT INTO state VALUES (NULL, 'Michigan', 'MI');
+INSERT INTO state VALUES (NULL, 'Minnesota', 'MN');
+INSERT INTO state VALUES (NULL, 'Mississippi', 'MS');
+INSERT INTO state VALUES (NULL, 'Missouri', 'MO');
+INSERT INTO state VALUES (NULL, 'Montana', 'MT');
+INSERT INTO state VALUES (NULL, 'Nebraska', 'NE');
+INSERT INTO state VALUES (NULL, 'Nevada', 'NV');
+INSERT INTO state VALUES (NULL, 'New Hampshire', 'NH');
+INSERT INTO state VALUES (NULL, 'New Jersey', 'NJ');
+INSERT INTO state VALUES (NULL, 'New Mexico', 'NM');
+INSERT INTO state VALUES (NULL, 'New York', 'NY');
+INSERT INTO state VALUES (NULL, 'North Carolina', 'NC');
+INSERT INTO state VALUES (NULL, 'North Dakota', 'ND');
+INSERT INTO state VALUES (NULL, 'Northern Mariana Islands', 'MP');
+INSERT INTO state VALUES (NULL, 'Ohio', 'OH');
+INSERT INTO state VALUES (NULL, 'Oklahoma', 'OK');
+INSERT INTO state VALUES (NULL, 'Oregon', 'OR');
+INSERT INTO state VALUES (NULL, 'Palau', 'PW');
+INSERT INTO state VALUES (NULL, 'Pennsylvania', 'PA');
+INSERT INTO state VALUES (NULL, 'Puerto Rico', 'PR');
+INSERT INTO state VALUES (NULL, 'Rhode Island', 'RI');
+INSERT INTO state VALUES (NULL, 'South Carolina', 'SC');
+INSERT INTO state VALUES (NULL, 'South Dakota', 'SD');
+INSERT INTO state VALUES (NULL, 'Tennessee', 'TN');
+INSERT INTO state VALUES (NULL, 'Texas', 'TX');
+INSERT INTO state VALUES (NULL, 'Utah', 'UT');
+INSERT INTO state VALUES (NULL, 'Vermont', 'VT');
+INSERT INTO state VALUES (NULL, 'Virgin Islands', 'VI');
+INSERT INTO state VALUES (NULL, 'Virginia', 'VA');
+INSERT INTO state VALUES (NULL, 'Washington', 'WA');
+INSERT INTO state VALUES (NULL, 'West Virginia', 'WV');
+INSERT INTO state VALUES (NULL, 'Wisconsin', 'WI');
+INSERT INTO state VALUES (NULL, 'Wyoming', 'WY');
+INSERT INTO state VALUES (NULL, 'Armed Forces Africa', 'AE');
+INSERT INTO state VALUES (NULL, 'Armed Forces Americas (except Canada)', 'AA');
+INSERT INTO state VALUES (NULL, 'Armed Forces Canada', 'AE');
+INSERT INTO state VALUES (NULL, 'Armed Forces Europe', 'AE');
+INSERT INTO state VALUES (NULL, 'Armed Forces Middle East', 'AE');
+INSERT INTO state VALUES (NULL, 'Armed Forces Pacific', 'AP');
